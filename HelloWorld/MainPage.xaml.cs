746ada2663fffe45f57caaee7b7d4e9101b25ee8
Owe Nordquist <john.doe@elisanet.fi>
2016-02-11 11:50:08 +0200

    testing

--- HelloWorld/MainPage.xaml.cs
+++ HelloWorld/MainPage.xaml.cs
@@ -25,6 +25,8 @@ namespace HelloWorld
         public MainPage()
         {
             this.InitializeComponent();
+                  
+
 
             this.NavigationCacheMode = NavigationCacheMode.Required;
         }
